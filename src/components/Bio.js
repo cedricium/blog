f47d56619133d14791e46299d7eb095be0eba0aa
Cedricium <amaya.cedric@gmail.com>
2018-12-23 13:21:08 -0800

    move bio avatar to assets/ directory

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,7 @@ import React from 'react'
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import profilePic from './avatar.png'
+import profilePic from '../assets/avatar.png'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
