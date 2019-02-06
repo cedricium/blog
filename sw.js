63186c3648b1a64f3ab6e91a0f1457a5bce50fc8
Cedricium <amaya.cedric@gmail.com>
2019-02-05 19:32:20 -0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8a43054109e1ebb9ea68.js"
+    "url": "webpack-runtime-7178ccfbeadc4370a725.js"
   },
   {
     "url": "app-18a095c74b1174a74089.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "461aa0054439b52d89bf61cc721508ed"
+    "revision": "37dc6dfae9a03d094831d7d771aa6e8a"
   },
   {
     "url": "0.1f4c3243659b557adae3.css"
