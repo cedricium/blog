1fe75e65e0387d5334293283643f7a065b994b10
Cedricium <amaya.cedric@gmail.com>
2020-02-25 15:05:16 -0800

    Fix: remove extra pathPrefix from sitemap urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,18 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            return {
+              url: (site.siteMetadata.siteUrl + edge.node.path).replace(/\/blog\/blog\//g, '/blog/'),
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          })
+      }
+    },
   ],
 }
